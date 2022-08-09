448cb975d6e9689d2a16ba68dbce6587fc6beb96
Vaibhav Gupta <guptavaibhav432@gmail.com>
2022-08-09 11:56:20 +0530

    Backend Route Updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vaibhav-notes-backend.herokuapp.com//api",
 });
 
 export default api;
